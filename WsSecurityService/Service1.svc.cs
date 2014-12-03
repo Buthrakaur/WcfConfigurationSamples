a460b54332a92dc5d7e5809236d3923e4fcb1000
Filip Kinský <filip@filovo.net>
2014-12-03 16:22:39 +0100

    WsSecurityService

---
+++ WsSecurityService/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IdentityModel.Selectors;
+using System.ServiceModel;
+
+namespace WsSecurityService
+{
+    public class Authentication : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+        }
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You '{1}' entered: {0}", value, OperationContext.Current.ServiceSecurityContext.PrimaryIdentity.Name);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
